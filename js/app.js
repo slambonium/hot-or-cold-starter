8262151e4e01e35c7bfeb1a55ab6de4766273305
Robert T. Stewart <bobstewart@goodtiming.com>
2015-04-03 18:04:15 -0400

    begins coding of newGame function; adds click response to start new game; adds function to generate a random number

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 
 $(document).ready(function(){
+
+  var guesses = 0, numToGuess;
+
+  // start a new game on completion of page load
+  newGame();
+
+  // https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers/onclick
+  document.querySelector('.new').onclick = newGame;
 	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
@@ -11,6 +19,21 @@ $(document).ready(function(){
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
+
+    // function newGame to start a new game
+    function newGame() {
+      guesses = 0;
+      document.querySelector('#count').innerHTML = guesses;
+      numToGuess = randomNumGen();
+      console.log(numToGuess);
+    }
+
+    // function randomNumGen will generate a random integer between 1 and max
+    function randomNumGen(max) {
+      if (max === undefined) {max = 100;}
+      var randomNum = Math.floor(Math.random() * max + 1);
+      return randomNum;
+    }
 
 });
 
